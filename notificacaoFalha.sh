00ed817a2e7f6eb33503c3ce7cdfb3f5a1d14770
jonilsonds9 <jonilsonds9@gmail.com>
2020-02-20 21:13:00 -0300

    adicionando slack

---
+++ notificacaoFalha.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X POST -H 'Content-type: application/json' --data '{"text":"ops! Algo deu errado"}' https://hooks.slack.com/services/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
---
+++ notificacaoSucesso.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X POST -H 'Content-type: application/json' --data '{"text":"tudo deu certo na pipeline!"}' https://hooks.slack.com/services/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
